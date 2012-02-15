c81484008832c9704b3688963cb00ffe29b3bc07
Alexander Dolidze <alexander.dolidze@gmail.com>
2012-02-15 06:51:43 +0400

    extracted a bit of information

--- rc.lua
+++ rc.lua
@@ -1,53 +1,23 @@
+-- LIBRARIES
 require("awful")
 require("awful.autofocus")
 require("awful.rules")
-require("wibox")
+--require("wibox")
 require("beautiful")
 require("naughty")
-require("vicious") 
+require("vicious")
 
---------------------------------------
+-- ERRORHANDLING
+require("10_errorhandling")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
+-- DEFAULTS
+terminal	= "xterm"
+editor		= os.getenv("EDITOR") or "nano"
+editor_cmd	= terminal .. " -e " .. editor
+browser		= "firefox"
+modkey		= "Mod4"
+altkey		= "Mod1"
 
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.add_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = err })
-        in_error = false
-    end)
-end
--- }}}
-
--- {{{ Variable definitions
--- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
-
--- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
-editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
 
 --require("01_colours")
 
